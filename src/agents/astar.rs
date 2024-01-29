f2d5c780f353a32a1b5a1468f4376ad1193d4584
nwrenger <nils@wrenger.net>
2024-01-29 19:00:27 +0100

    :sparkles: Added move decider && Deleted AStar to enemy head (+modes)

--- src/agents/astar.rs
+++ src/agents/astar.rs
@@ -12,68 +12,41 @@ use crate::grid::CellT;
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct StarAgent;
 
-enum Mode {
-    Food,
-    Kill(usize),
-}
-
 impl StarAgent {
     pub async fn step(&self, game: &Game) -> MoveResponse {
         let my = &game.snakes[0];
-        let mut mode = Mode::Food;
-        for i in 1..game.snakes.len() {
-            if game.snakes[i].alive()
-                && my.body.len() > 10
-                && my.body.len() > game.snakes[i].body.len() + 3
-                && (game.snakes[i].head().x - my.head().x).abs() <= 1
-                && (game.snakes[i].head().y - my.head().y).abs() <= 1
-            {
-                mode = Mode::Kill(i);
+
+        let mut food = Vec::new();
+        for y in 0..game.grid.height as i16 {
+            for x in 0..game.grid.width as i16 {
+                if game.grid[v2(x, y)].t == CellT::Food {
+                    food.push(v2(x, y));
+                }
             }
         }
 
-        let target = match mode {
-            Mode::Food => {
-                let mut food = Vec::new();
-                for y in 0..game.grid.height as i16 {
-                    for x in 0..game.grid.width as i16 {
-                        if game.grid[v2(x, y)].t == CellT::Food {
-                            food.push(v2(x, y));
-                        }
-                    }
-                }
-
-                food.iter()
-                    .min_by(|&&a, &&b| {
-                        let distance_a =
-                            ((a.x - my.head().x).pow(2) + (a.y - my.head().y).pow(2)) as f64;
-                        let distance_b =
-                            ((b.x - my.head().x).pow(2) + (b.y - my.head().y).pow(2)) as f64;
-                        distance_a
-                            .partial_cmp(&distance_b)
-                            .unwrap_or(Ordering::Equal)
-                    })
-                    .copied()
-            }
-            Mode::Kill(i) => Some(game.snakes[i].head()),
-        };
-
-        match target {
-            Some(target) => {
-                let path = game.grid.a_star(my.head(), target, &[0.0, 0.0, 0.0, 0.0]);
-                match path {
-                    Some(path) => {
-                        if path.len() >= 2 {
-                            MoveResponse::new(Direction::from(path[1] - path[0]))
-                        } else {
-                            MoveResponse::new(random(game))
-                        }
-                    }
-                    None => MoveResponse::new(random(game)),
+        if let Some(target) = food
+            .iter()
+            .min_by(|&&a, &&b| {
+                let distance_a = ((a.x - my.head().x).pow(2) + (a.y - my.head().y).pow(2)) as f64;
+                let distance_b = ((b.x - my.head().x).pow(2) + (b.y - my.head().y).pow(2)) as f64;
+                distance_a
+                    .partial_cmp(&distance_b)
+                    .unwrap_or(Ordering::Equal)
+            })
+            .copied()
+        {
+            if let Some(path) = game.grid.a_star(my.head(), target, &[0.0, 0.0, 0.0, 0.0]) {
+                if path.len() >= 2 {
+                    return MoveResponse::new(move_check(
+                        game,
+                        Direction::from(path[1] - path[0]),
+                        &mut None,
+                    ));
                 }
             }
-            None => MoveResponse::new(random(game)),
         }
+        MoveResponse::new(random(game, &mut None))
     }
 }
 
@@ -81,7 +54,43 @@ thread_local! {
     static RNG: RefCell<SmallRng> = RefCell::new(SmallRng::from_entropy())
 }
 
-fn random(game: &Game) -> Direction {
-    let moves = game.valid_moves(0);
-    RNG.with_borrow_mut(|rng| moves.choose(rng).unwrap_or(Direction::Up))
+fn random(game: &Game, nots: &mut Option<Vec<Direction>>) -> Direction {
+    let mut moves = game.valid_moves(0).collect::<Vec<Direction>>();
+    if let Some(nots) = nots {
+        for not in nots {
+            moves.retain(|dir| *dir != *not);
+        }
+    }
+    if moves.is_empty() {
+        return *game
+            .valid_moves(0)
+            .collect::<Vec<Direction>>()
+            .first()
+            .unwrap_or(&Direction::Up);
+    }
+    move_check(
+        game,
+        *RNG.with_borrow_mut(|rng| moves.iter().choose(rng).unwrap_or(&Direction::Up)),
+        nots,
+    )
+}
+
+fn move_check(game: &Game, r#move: Direction, nots: &mut Option<Vec<Direction>>) -> Direction {
+    let my = &game.snakes[0];
+    let future_pos = my.head().apply(r#move);
+    for snake in &game.snakes[1..] {
+        if snake.body.len() >= my.body.len()
+            && Direction::all()
+                .iter()
+                .any(|dir| snake.head().apply(*dir) == future_pos)
+        {
+            match nots {
+                Some(nots) => nots.push(r#move),
+                None => *nots = Some(vec![r#move]),
+            }
+            return random(game, nots);
+        }
+    }
+
+    r#move
 }
